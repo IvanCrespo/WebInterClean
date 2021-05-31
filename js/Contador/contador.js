ddf162d690cb7818ab87e39f641c80567057c1c5
Cesar <c3za1.97@gmail.com>
2021-05-31 17:41:58 -0500

    reparacion v1 contador

--- js/Contador/contador.js
+++ js/Contador/contador.js
@@ -1,15 +1,20 @@
 function contador() {
-    const counters = document.querySelectorAll('.counter');
-    const speed = 100;
+const counters = document.querySelectorAll('.counter');
+const speed = 100;
     counters.forEach(counter => {
-    const updateCount = () => {
-    const target = +counter.getAttribute('data-target');
-    const count = +counter.innerText;
-    const inc = target / speed;
-    if (count < target) {
-    counter.innerText = count + inc;
-    setTimeout(updateCount, 1);
-    } else {
-    counter.innerText = target;}};
-    updateCount();});
+            const updateCount = () => {
+                
+            const target = +counter.getAttribute('data-target');
+            const count = +counter.innerText;
+            const inc = target / speed;
+
+            if (count < target) {
+            counter.innerText = count + inc;
+            setTimeout(updateCount, 1);
+            } else {
+            counter.innerText = target;
+            }
+        };
+    updateCount();
+    });
 }
