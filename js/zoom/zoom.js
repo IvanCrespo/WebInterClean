d9a5b701caa7f2b3384f0da2a977226d9a3a5671
JCAPDEV <aguila08ruso2@gmail.com>
2021-06-03 18:05:08 -0500

    Formularios 95%

--- js/zoom/zoom.js
+++ js/zoom/zoom.js
@@ -1,6 +1,6 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    console.log(scroll);
+    // console.log(scroll);
       $(".zoom img").css({
           transform: 'translate3d(-50%, -'+(scroll/100)+'%, 0) scale('+(100 + scroll)/100+')',
           opacity: 1-(scroll/800),
