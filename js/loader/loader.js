64bedc9ad50bb50b29358b7d7373896a8e45962b
IvanCrespo <utp0003791@alumno.utpuebla.edu.mx>
2021-06-03 12:19:43 -0500

    Página Web 75%

---
+++ js/loader/loader.js
@@ -0,0 +1,12 @@
+// Loading Initial Page
+$('body').append('<div class="loader-container"><div id="loading"></div></div>');
+$(window).on('load', function(){
+  setTimeout(removeLoader, 2 * 3000);
+});
+function removeLoader(){
+    $( ".loader-container" ).fadeOut(500, function() {
+      $( ".loader-container" ).remove();
+      // window.location="https://www.facebook.com/";
+    });
+}
+// Fin Loading Initial Page
