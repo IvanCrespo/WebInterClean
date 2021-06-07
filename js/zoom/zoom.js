1214ab057b4c924d8a23aeca2e82c2cc857e458f
IvanCrespo <utp0003791@alumno.utpuebla.edu.mx>
2021-06-07 18:06:15 -0500

    Query Celular Index 70%

--- js/zoom/zoom.js
+++ js/zoom/zoom.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     // console.log(scroll);
-      $(".zoom img").css({
+      $(".zoom .original").css({
           transform: 'translate3d(-50%, -'+(scroll/100)+'%, 0) scale('+(100 + scroll)/100+')',
           opacity: 1-(scroll/800),
           
