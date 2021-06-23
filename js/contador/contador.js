957378269c573cd43f86b0d66eb11e04b3ff4b84
IvanCrespo <utp0003791@alumno.utpuebla.edu.mx>
2021-06-23 17:45:43 -0500

    Cambios

--- js/contador/contador.js
+++ js/contador/contador.js
@@ -1,29 +1,29 @@
-addEventListener('DOMContentLoaded', ()=>{
+addEventListener('DOMContentLoaded', () => {
     const contadores = document.querySelectorAll('.contador_cantidad')
     const velocidad = 9000
 
     const animarContadores = () => {
-        for (const contador of contadores) {
-            const actualizar_contador = () => {
-                let cantidad_maxima = +contador.dataset.cantidadTotal,
-                valor_actual = +contador.innerText,
-                incremento = cantidad_maxima / velocidad
+            for (const contador of contadores) {
+                const actualizar_contador = () => {
+                    let cantidad_maxima = +contador.dataset.cantidadTotal,
+                        valor_actual = +contador.innerText,
+                        incremento = cantidad_maxima / velocidad
 
-                if(valor_actual < cantidad_maxima){
-                    contador.innerText = Math.ceil(valor_actual + incremento)
-                    setTimeout(actualizar_contador, 5)
-                } else {
-                    contador.innerText = cantidad_maxima
+                    if (valor_actual < cantidad_maxima) {
+                        contador.innerText = Math.ceil(valor_actual + incremento)
+                        setTimeout(actualizar_contador, 5)
+                    } else {
+                        contador.innerText = cantidad_maxima
+                    }
                 }
+                actualizar_contador()
             }
-            actualizar_contador() 
         }
-    }
-    // IntersectionObserver
+        // IntersectionObserver
 
     const mostrarContadores = elementos => {
         elementos.forEach(elemento => {
-            if(elemento.isIntersecting) {
+            if (elemento.isIntersecting) {
                 elemento.target.classList.add('animar')
                 elemento.target.classList.remove('ocultar')
                 setTimeout(animarContadores, 500)
