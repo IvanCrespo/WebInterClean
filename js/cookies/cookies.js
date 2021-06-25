df6a8e4ddca16b466cfccdd3c6d84d58dc6cf2d5
IvanCrespo <utp0003791@alumno.utpuebla.edu.mx>
2021-06-25 17:12:13 -0500

    Cookies y permisos 100% (Faltan las img y contenidos, ya esta el de loader)

--- js/cookies/cookies.js
+++ js/cookies/cookies.js
@@ -1,21 +1,37 @@
-const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
-const avisoCookies = document.getElementById('aviso-cookies');
-const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
+$(document).ready(function() {
+    $(window).load(function() {
 
-dataLayer = [];
+        const botonAceptarCookies = document.getElementById('btn-aceptar-cookies');
+        const avisoCookies = document.getElementById('aviso-cookies');
+        const fondoAvisoCookies = document.getElementById('fondo-aviso-cookies');
 
-if (!localStorage.getItem('cookies-aceptadas')) {
-    avisoCookies.classList.add('activo');
-    fondoAvisoCookies.classList.add('activo');
-} else {
-    dataLayer.push({ 'event': 'cookies-aceptadas' });
-}
+        function Preloader() {
+            var preloader = $('.preloader');
+            preloader.delay(7000).fadeOut('slow');
+        }
 
-botonAceptarCookies.addEventListener('click', () => {
-    avisoCookies.classList.remove('activo');
-    fondoAvisoCookies.classList.remove('activo');
+        dataLayer = [];
 
-    localStorage.setItem('cookies-aceptadas', true);
+        if (!localStorage.getItem('cookies-aceptadas')) {
+            avisoCookies.classList.add('activo');
+            fondoAvisoCookies.classList.add('activo');
 
-    dataLayer.push({ 'event': 'cookies-aceptadas' });
+            Preloader();
+        } else {
+            $('.preloader').hide();
+            dataLayer.push({ 'event': 'cookies-aceptadas' });
+        }
+
+        botonAceptarCookies.addEventListener('click', () => {
+            avisoCookies.classList.remove('activo');
+            fondoAvisoCookies.classList.remove('activo');
+
+            sessionStorage.setItem('.preloader', true);
+
+            localStorage.setItem('cookies-aceptadas', true);
+
+
+            dataLayer.push({ 'event': 'cookies-aceptadas' });
+        });
+    });
 });
