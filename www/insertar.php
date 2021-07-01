485675ef88551def40a595f37b4e479b2dba4f83
Luis-Enrique <luiswetowkn@gmail.com>
2021-07-01 14:32:46 -0500

    estado/ciudad

--- www/insertar.php
+++ www/insertar.php
@@ -5,6 +5,8 @@ $conexion = mysqli_connect("localhost", "intercle_admin", "Soporte=2020-2", "int
 $nombreCompleto = $_POST["nombreCompleto"];
 $telefono = $_POST["telefono"];
 $vacantes = $_POST["vacantes"];
+$estado = $_POST["estado"];
+$ciudad = $_POST["ciudad"];
 //Form Cotizacion 
 $correo= $_POST["correo"]; 
 $telefono= $_POST["telefono"]; 
@@ -14,7 +16,7 @@ $mensaje= $_POST["mensaje"];
 
     //Insert DATA;
     //Form Empleado
-        $insertarSQL = "INSERT INTO WebInempleos(nombreCompleto, telefono, vacantes) VALUES ('$nombreCompleto', '$telefono', '$vacantes')";
+        $insertarSQL = "INSERT INTO WebInempleos(nombreCompleto, telefono, vacantes, estado, ciudad) VALUES ('$nombreCompleto', '$telefono', '$vacantes', '$estado', '$ciudad')";
         $resultado = mysqli_query($conexion, $insertarSQL);
         if ($resultado) {
             echo '<script type="text/javascript">window.alert("se ha enviado correctamente"); window.location.href="empleo.html";</script>';
