dd53c810b1e72773e02c5c08bad00818dc6357e9
Luis-Enrique <luiswetowkn@gmail.com>
2021-07-01 12:14:05 -0500

    rutas

--- www/insertar.php
+++ www/insertar.php
@@ -2,20 +2,21 @@
 //Conexion
 $conexion = mysqli_connect("localhost", "intercle_admin", "Soporte=2020-2", "intercle_intranet");
 //Datos Personales
-$nombre = $_POST["nombre"];
+$nombreCompleto = $_POST["nombreCompleto"];
 $telefono = $_POST["telefono"];
-$vacante = $_POST["vacante"];
+$vacantes = $_POST["vacantes"];
 
 
 
     //Insert DATA;
-        $insertarSQL = "INSERT INTO WebInempleos(nombre, telefono, vacante) VALUES ('$nombre', '$telefono', '$vacante')";
+        $insertarSQL = "INSERT INTO WebInempleos(nombreCompleto, telefono, vacantes) VALUES ('$nombreCompleto', '$telefono', '$vacantes')";
         $resultado = mysqli_query($conexion, $insertarSQL);
         if ($resultado) {
             echo '<script type="text/javascript">window.alert("se ha enviado correctamente");</script>';
 
         } else{
             printf("Errormessage: %s\n", mysqli_error($conexion));
+            echo "Error";
         }
         
 
